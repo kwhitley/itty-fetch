1e0ebf0c60738c7651bd37b3af3d70fc32f7679f
Kevin Whitley <krwhitley@gmail.com>
2024-04-23 15:12:58 -0500

    headers blending

--- src/fetcher.spec.ts
+++ src/fetcher.spec.ts
@@ -1,28 +1,90 @@
 import { describe, expect, it, vi } from 'vitest'
 import { fetcher } from './fetcher'
 
+const OBJECT = { foo: 'bar' }
+const STRINGIFIED_OBJECT = JSON.stringify(OBJECT)
+
+const fetch = (spy) => (request) => {
+  spy(request)
+
+  return Promise.resolve(new Response(STRINGIFIED_OBJECT))
+}
+
 describe('fetcher', () => {
-  it('fetcher.post() => calling post with id = 3', async () => {
-    const fetch = vi.fn(v => v)
-    await fetcher({ fetch }).post()
-    expect(fetch).toHaveReturnedWith('')
+  it('fetcher.post() => calls a request with method POST', async () => {
+    const spy = vi.fn(r => r.method)
+
+    const response = await fetcher({ fetch: fetch(spy) }).post()
+    expect(spy).toHaveReturnedWith('POST')
+    expect(response).toEqual(OBJECT)
   })
 
-  it('fetcher(options).post() => will call fetcher with options', async () => {
-    const fetch = vi.fn((_, options) => options)
-    await fetcher({ fetch, id: 3 }).post()
-    expect(fetch).toHaveReturnedWith({ id: 3 })
+  it('fetcher({ base }).get() => prepends base to path', async () => {
+    const spy = vi.fn(r => r.url)
+
+    await fetcher({ base: 'https:foo.bar', fetch: fetch(spy) }).post()
+    expect(spy).toHaveReturnedWith('https://foo.bar/')
   })
 
-  it('fetcher(options1).post(options2) => will call fetcher with options2', async () => {
-    const fetch = vi.fn((_, options) => options)
-    await fetcher({ fetch }).post(null, { id: 3 })
-    expect(fetch).toHaveReturnedWith({ id: 3 })
+  it('fetcher({ headers: {} }).get() => appends headers to request', async () => {
+    const spy = vi.fn(r => r.headers.get('foo'))
+
+    await fetcher({
+      base: 'https:foo.bar',
+      fetch: fetch(spy),
+      headers: { foo: 'bar' },
+    }).get('/cats')
+    expect(spy).toHaveReturnedWith('bar')
   })
 
-  it('fetcher(options1).post(options2) => will blend options', async () => {
-    const fetch = vi.fn((_, options) => options)
-    await fetcher({ fetch, foo: 'bar' }).post(null, { id: 3 })
-    expect(fetch).toHaveReturnedWith({ foo: 'bar', id: 3 })
+  it('fetcher({ headers: {} }).get() => appends headers to request', async () => {
+    const spy = vi.fn(r => [...r.headers.entries()])
+    const headers = new Headers()
+    headers.append('foo', 'bar')
+
+    await fetcher({
+      base: 'https:foo.bar',
+      fetch: fetch(spy),
+      headers,
+    }).get('/cats')
+    expect(spy).toHaveReturnedWith([
+      ['foo', 'bar']
+    ])
   })
+
+  it('fetcher({ headers }).get({ headers }) => blends base headers with final ones', async () => {
+    const spy = vi.fn(r => [...r.headers.entries()])
+    const headers = new Headers()
+    headers.append('foo', 'bar')
+
+    await fetcher({
+      base: 'https:foo.bar',
+      fetch: fetch(spy),
+      headers: { foo: 'bar', cat: 'dog' },
+    }).get('/cats', null, {
+      headers: { foo: 'baz' }
+    })
+    expect(spy).toHaveReturnedWith([
+      ['cat', 'dog'],
+      ['foo', 'baz'],
+    ])
+  })
+
+  // it('fetcher(options).post() => will call fetcher with options', async () => {
+  //   const fetch = vi.fn((_, options) => options)
+  //   await fetcher({ fetch, id: 3 }).post()
+  //   expect(fetch).toHaveReturnedWith({ id: 3 })
+  // })
+
+  // it('fetcher(options1).post(options2) => will call fetcher with options2', async () => {
+  //   const fetch = vi.fn((_, options) => options)
+  //   await fetcher({ fetch }).post(null, { id: 3 })
+  //   expect(fetch).toHaveReturnedWith({ id: 3 })
+  // })
+
+  // it('fetcher(options1).post(options2) => will blend options', async () => {
+  //   const fetch = vi.fn((_, options) => options)
+  //   await fetcher({ fetch, foo: 'bar' }).post(null, { id: 3 })
+  //   expect(fetch).toHaveReturnedWith({ foo: 'bar', id: 3 })
+  // })
 })
--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,17 +1,27 @@
 type Options = {
   base?: string
   fetch?: Function
+  headers?: Headers | Record<string, any>
 }
 
+type FetchCallOptions = {
+  parse?: false | Function
+  encode?: false | Function
+  body?: any
+} & RequestInit
+
+type FetchCall = (url: string, payload: any, options: FetchCallOptions) => Promise<any>
+
 const createEnhancedFunction = ({
-  base = '',
+  base = window?.location?.origin ?? '',
   fetch = () => {},
-  ...other
+  headers = {},
+  ...options
 }: Options = {}) => new Proxy((o: any) => createEnhancedFunction(o), {
-  get(obj: any, prop: any) {
-    return obj[prop]
+  get(obj: any, method: any) {
+    return obj[method]
       ?? (
-        (...args: any) => {
+        (...args: any): FetchCall => {
           // extract base
           base = base + (
             typeof args[0] == 'string'
@@ -19,16 +29,59 @@ const createEnhancedFunction = ({
               : ''
           )
 
-          const payload = args.shift(),
-                options = args.shift()
+          // extract payload
+          let payload = args.shift()
 
-          return fetch(base, {
-            ...other,
-            ...options,
-          })
+          options.method = method
+
+
+          // created blended options
+          options = { ...options, ...args.shift() }
+
+          // turn base headers into actual headers instance
+          // headers = new Headers(headers)
+
+          const {
+            parse = (r: any) => r.json(),
+            // encode = JSON.stringify,
+          } = options as FetchCallOptions
+
+          if (payload) {
+            if (typeof payload != 'string') {
+              payload = JSON.stringify(payload)
+              headers.set('content-type', 'application/json')
+            }
+            options.body = payload
+          }
+
+          // create request
+          const request = new Request(base, options)
+
+          // add base
+          // for (const [key, value] of headers.entries()) {
+          //   request.headers.set(key, value)
+          // }
+
+          // append any headers
+          for (const [key, value] of [...new Headers(headers).entries(), ...request.headers.entries()]) {
+            request.headers.set(key, value)
+          }
+
+          // console.log('request headers are', [...request.headers.entries()])
+
+          let fetchCall = fetch(request)
+
+          if (parse) {
+            fetchCall = fetchCall.then(parse)
+          }
+
+          return fetchCall
         }
       )
   }
 })
 
 export const fetcher = createEnhancedFunction()
+
+
+// fetcher({ base: 'test' })
